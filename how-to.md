449b107d9343feb36123de51da56e280e50bec7f
Julien Marie-Magdeleine <julien.p.mmagdeleine@gmail.com>
2024-04-08 11:22:24 +0200

    Update info guide on test repo, updated gisting test script

--- how-to.md
+++ how-to.md
@@ -1,2 +1,60 @@
 # Structure
-In this directory are the result files for testing
+This is the testing directory. It consists of 3 main directories.
+- test-files
+- test-scripts
+- test-results
+
+## Test files
+It contains the test files used. Naming standards will change, it only contains budget-test and its different resolutions for now. The cats-vs-dogs dataset was not included, but it can be found on Kaggle.
+
+**Tests run on the model**:
+- **Two-class classification**: cats vs. dogs
+- **Spreadsheet comprehension**: budget test
+  - _Gisting_: How well the model can understand what the different rows and columns correspond to
+  - _Info retrieval_: How well the model has understood what it's looking at
+  - _Data processing_: On top of understanding, can it think about what it's looking at?
+
+## Test scripts
+These are the scripts used for each test. Since there were only two, two scripts are present, one for each main test.
+
+The scripts output the raw results in .csv format, recording the prompts.
+
+### Cats vs. dogs:
+- Prompt: "What animal do you see in this photo? Answer with either Cat or Dog."
+- Results: table in the [pass/fail, file name] format.
+The data processing is done with the test for this one.
+
+### Spreadsheet:
+
+- #### Gisting:
+- Prompt: "What does the [nth] [column/row] do?"
+- Results: table in the [recorded answer, test time] format
+Maybe some modifications will be made to the prompt to get better answers.
+
+- #### Information retrieval:
+
+To be implemented.
+
+- #### Data processing:
+
+To be implemented
+
+## Test results
+This directory contains all test results in csv format and have a results processing script, to process the raw data stored for each test. The scripts currenty need to be implemented for each type of test.
+
+### Cats vs. dogs
+Tested on vicuna 13b. It does really well in terms of classification, but can sometimes be amiss for instruction following. Instead of answering with "dog", as asked, it sometimes will answer with puppy.
+
+### Spreadsheet
+The vicuna 13b and 34b model were tested. The first to be tested is a low-res image, up to HD image, with an intermediary med-res image. This is to see if there is a resolution, starting from which/up which to which it works. There is then an adapted resolution, made so that it fits an integer number of patches for ViT.
+
+- #### Gisting:
+13b does poorly, oftentimes confusing columns for others, and rows for other columns. Hallucinates rows if not indicated whether to think about if the row is there or not. It consistently does those errors: It is reasonable to think that the resolution has little to no effect on error.
+
+34b does much better, being very thorough and precise in the information it gives back.
+
+- #### Information retrieval:
+TBD
+
+- #### Data processing:
+TBD
--- test-scripts/gisting.py
+++ test-scripts/gisting.py
@@ -1,27 +1,32 @@
 # Module imports for testing
 import time, sys
 import pandas as pd
+import torch
 
 # Model imports
 from llava.model.builder import load_pretrained_model
 from llava.mm_utils import get_model_name_from_path
-from llava.eval.run_llava_copy import eval_model
+from llava.eval.run_llava_test import eval_model
+
+# Interpret command line inputs
+model_descriptor = sys.argv[1]
+image_descriptor = sys.argv[2]
 
 # Helper functions
 def generate_prompt(line_or_column, location):
   return prompt_skeleton1 + location + line_or_column + prompt_skeleton2
 
 # Loading model
-if sys.argv[1] == 'light':
+if model_descriptor == 'light':
   model_path = "liuhaotian/llava-v1.6-vicuna-13b"
-elif sys.argv[1] == 'full':
+elif model_descriptor == 'full':
   model_path = "liuhaotian/llava-v1.6-34b"
 
 tokenizer, model, image_processor, context_len = load_pretrained_model(
     model_path=model_path,
     model_base=None,
     model_name=get_model_name_from_path(model_path),
-    load_4bit = True
+    load_4bit=True
 )
 
 # LLaVA globals
@@ -29,13 +34,12 @@ locations = ["first", "second", "third", "fourth", "fifth"]
 line_or_column = [" column", " line"]
 prompt_skeleton1 = "What does the "
 prompt_skeleton2 = " correspond to?"
-  # Choose between budget-test.png, budget-test-med-res.png, budget-test-high-res.png and budget-test-adapted-res.png
-image_file_dir = "/home/jmarie/test-files/budget-test-adapted-res.png"
 
+treated_descriptor = "-adapted-res" if image_descriptor == "adapted" else ("-high-res" if image_descriptor == "high" else ("-med-res" if image_descriptor == "med" else ""))
+image_file_dir = "/home/jmarie/test-files/budget-test" + treated_descriptor + ".png"
+
+model_string = get_model_name_from_path(model_path).replace("llava-v1.6-", "")
 args = type('Args', (), {
-  # Useless arguments that would be used to load the model in old eval_model func
-  #  "model_path": model_path,
-  #  "model_base": None,
     "model_name": get_model_name_from_path(model_path),
     "query": "",
     "conv_mode": None,
@@ -45,7 +49,7 @@ args = type('Args', (), {
     "top_p": None,
     "num_beams": 1,
     "max_new_tokens": 30,
-  # Arguments import from eval_model so that we don't have to load pretrained model each time
+  # Arguments import from eval_model to avoid loading the model with each try
     "tokenizer": tokenizer,
     "model": model,
     "image_processor": image_processor,
@@ -68,4 +72,4 @@ end_time = time.time()
 
 print("Test time -----------------> ", end_time - start_time, "s")
 outFrame = pd.DataFrame(data = [[output, gen_time] for output,gen_time in zip(outputs,gen_times)], columns = ["Outputs", "Generation time"])
-outFrame.to_csv('/home/jmarie/test-results/gisting-' + get_model_name_from_path(model_path) + '-adapted-res.csv')
+outFrame.to_csv('/home/jmarie/test-results/gisting/gisting-' + model_string + treated_descriptor + '.csv')
